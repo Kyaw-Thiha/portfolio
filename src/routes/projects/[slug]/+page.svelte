d8d15881ee824a451d449a30581784a106dc854d
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-01-04 02:00:14 +0630

    Added IGTools content

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MyButton from '$lib/components/myButton.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -7,8 +8,13 @@
 <section class="project">
 	<h1>{data.project?.name}</h1>
 	<div class="img-container"><img src={data.project?.image} alt={data.project?.name} /></div>
+	<div class="btn-container">
+		<a href={data.project?.link} target="_blank"> <MyButton text="Visit Website" /> </a>
+	</div>
 
-	<p>{data.project?.detail}</p>
+	<p class="project-detail">
+		{@html data.project?.detail}
+	</p>
 </section>
 
 <style lang="scss" scoped>
@@ -25,13 +31,34 @@
 
 			img {
 				width: 700px;
+				border-radius: 25px;
 			}
 		}
 
-		p {
+		.btn-container {
 			margin-top: 32px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+
+		.project-detail {
+			margin-top: 64px;
+		}
+
+		.project-detail > :global(h2) {
+			font-size: 32px;
+		}
+
+		.project-detail > :global(h3) {
+			font-weight: 400;
+			font-size: 28px;
+		}
+
+		.project-detail > :global(p) {
 			font-size: 20px;
 			text-align: justify;
+			font-weight: 300;
 		}
 	}
 </style>
