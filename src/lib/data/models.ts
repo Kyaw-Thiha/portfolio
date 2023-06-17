7733d30d521282184d88d2b40252b7ab35bab261
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-17 16:24:16 +0630

    Added parameter types

--- src/lib/data/models.ts
+++ src/lib/data/models.ts
@@ -6,7 +6,14 @@ export class Project {
 	detail: string;
 	technologies: string[];
 
-	constructor(name, image, description, link, detail, technologies) {
+	constructor(
+		name: string,
+		image: string,
+		description: string,
+		link: string,
+		detail: string,
+		technologies: string[]
+	) {
 		this.name = name;
 		this.image = image;
 		this.description = description;
