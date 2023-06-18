80b7be06bcf5741231d2817fcae2ca2a00574ee2
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-18 19:44:25 +0630

    Extended tailwind box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			boxShadow: {
+				card: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+			}
+		}
 	},
 	plugins: []
 };
