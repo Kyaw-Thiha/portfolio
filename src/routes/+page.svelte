c7b51d650b9f1c739904f59f652fcba97f321add
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-18 22:04:42 +0630

    Reordered the classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,10 +18,10 @@
 	<meta name="description" content="Portfolio Website of Kyaw Thiha @ Kevin" />
 </svelte:head>
 
-<section class="flex flex-row items-center justify-center">
+<section class="mt-8 flex flex-col items-center justify-center md:mt-0 md:flex-row">
 	<img width="400" class="rounded-2xl" src={profile} alt="My Profile" />
-	<div class="ml-12">
-		<h2 class="text-5xl mb-4 font-semibold">Mingalabar 👋</h2>
+	<div class="mt-4 text-center md:ml-12 md:mt-0 md:text-left">
+		<h2 class="mb-4 text-5xl font-semibold">Mingalabar 👋</h2>
 		<p class="text-2xl leading-relaxed">
 			My name is <b>Kevin</b>. I am a {age} years old who loves finding way to solve problems around
 			me by using technology.
@@ -30,17 +30,21 @@
 </section>
 
 <section id="projects" class="mt-20">
-	<h2 class="text-4xl mb-12 text-center">My Projects</h2>
+	<h2 class="mb-12 text-center text-4xl">My Projects</h2>
 
-	<div class="project-cards grid grid-cols-2 gap-12">
+	<div class="project-cards grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12">
 		{#each projects as project}
-			<div class="h-full shadow-card rounded-3xl px-4 py-4">
-				<h3 class="text-3xl mb-4 text-center font-semibold">{project.name}</h3>
+			<div class="h-full rounded-3xl px-4 py-4 shadow-card">
+				<h3 class="mb-4 text-center text-3xl font-semibold">{project.name}</h3>
 				<div class="flex items-center justify-center">
-					<img class="h-[250px]" src={project.image} alt={project.name} />
+					<img
+						class="h-[250px] object-cover md:object-contain"
+						src={project.image}
+						alt={project.name}
+					/>
 				</div>
-				<div class="text-center flex flex-col mt-2">
-					<h4 class="text-center text-lg mb-4">{project.description}</h4>
+				<div class="mt-2 flex flex-col text-center">
+					<h4 class="mb-4 text-center text-lg">{project.description}</h4>
 					<a href={`/projects/${project.name}`}> <MyButton text={'Learn More'} /></a>
 				</div>
 			</div>
