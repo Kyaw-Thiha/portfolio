206f05954b821394c131ca81bba06c34cbd74aa4
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-18 20:13:16 +0630

    Changed the styling to tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,127 +18,32 @@
 	<meta name="description" content="Portfolio Website of Kyaw Thiha @ Kevin" />
 </svelte:head>
 
-<section class="hero">
-	<img class="profile-img" src={profile} alt="My Profile Image" />
-	<div class="hero-text">
-		<h2>Mingalabar 👋</h2>
-		<p>
+<section class="flex flex-row items-center justify-center">
+	<img width="400" class="rounded-2xl" src={profile} alt="My Profile" />
+	<div class="ml-12">
+		<h2 class="text-5xl mb-4 font-semibold">Mingalabar 👋</h2>
+		<p class="text-2xl leading-relaxed">
 			My name is <b>Kevin</b>. I am a {age} years old who loves finding way to solve problems around
 			me by using technology.
 		</p>
 	</div>
 </section>
 
-<section id="projects" class="projects">
-	<h2>My Projects</h2>
+<section id="projects" class="mt-20">
+	<h2 class="text-4xl mb-12 text-center">My Projects</h2>
 
-	<div class="project-cards">
+	<div class="project-cards grid grid-cols-2 gap-12">
 		{#each projects as project}
-			<div class="project-card">
-				<div class="project-card-container">
-					<div class="project-card-contents">
-						<h3>{project.name}</h3>
-						<img src={project.image} alt={project.name} />
-						<h4>{project.description}</h4>
-						<div class="btn">
-							<a href={`/projects/${project.name}`}> <MyButton text={'Learn More'} /></a>
-						</div>
-					</div>
+			<div class="h-full shadow-card rounded-3xl px-4 py-4">
+				<h3 class="text-3xl mb-4 text-center font-semibold">{project.name}</h3>
+				<div class="flex items-center justify-center">
+					<img class="h-[250px]" src={project.image} alt={project.name} />
+				</div>
+				<div class="text-center flex flex-col mt-2">
+					<h4 class="text-center text-lg mb-4">{project.description}</h4>
+					<a href={`/projects/${project.name}`}> <MyButton text={'Learn More'} /></a>
 				</div>
 			</div>
 		{/each}
 	</div>
 </section>
-
-<style lang="scss" scoped>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	.hero {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-
-		.profile-img {
-			width: 400px;
-			border-radius: 32px;
-		}
-
-		.hero-text {
-			margin-left: 48px;
-
-			h2 {
-				color: black;
-				font-size: 60px;
-				font-weight: 600;
-			}
-
-			p {
-				font-size: 24px;
-			}
-		}
-	}
-
-	.projects {
-		margin-top: 48px;
-
-		h2 {
-			font-size: 40px;
-		}
-
-		.project-cards {
-			width: 100%;
-			margin-top: 32px;
-			display: flex;
-			flex-direction: row;
-			justify-content: center;
-			align-items: center;
-			flex-wrap: wrap;
-
-			.project-card {
-				width: 50%;
-				margin-top: 50px;
-
-				.project-card-container {
-					display: flex;
-					flex-direction: row;
-					justify-content: center;
-					align-items: center;
-
-					.project-card-contents {
-						border-radius: 32px;
-						box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-						padding: 0 16px;
-					}
-				}
-
-				h3 {
-					font-size: 32px;
-					text-align: center;
-				}
-
-				img {
-					width: 300px;
-					max-height: 250px;
-				}
-
-				h4 {
-					font-size: 16px;
-					text-align: center;
-				}
-
-				.btn {
-					text-align: center;
-					margin-bottom: 18px;
-					margin-top: 12px;
-				}
-			}
-		}
-	}
-</style>
