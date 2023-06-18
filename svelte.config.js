76b3932f1e9001a24f9a60cbdb4e886d721a1d65
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-18 15:49:32 +0630

    Integrated tailwind css

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 import importAssets from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess(), importAssets()],
-
+	preprocess: [preprocess(), importAssets(), vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
