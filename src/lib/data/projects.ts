284e27911aeb644d8097d202917210c2534949c2
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-03-16 13:05:40 +0630

    Removed svelte files

--- src/lib/data/projects.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from '$lib/data/models';
-import { IGTools } from '$lib/data/IGTools';
-import { BookKay } from '$lib/data/BookKay';
-import { IIPFunFair } from '$lib/data/IIPFunFair';
-import { Portfolio } from '$lib/data/Portfolio';
-import { IIPWebsite } from '$lib/data/IIPWebsite';
-import { IIPAdminWebsite } from '$lib/data/IIPAdminWebsite';
-
-export const projects = [IIPAdminWebsite, IIPWebsite, Portfolio, IIPFunFair, BookKay, IGTools];
