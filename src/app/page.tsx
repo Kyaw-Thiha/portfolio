1447a2f147a03696b4b0f15c19c6bb2fe7de39b1
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-03-16 23:36:06 +0630

    Partially implemented about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
-import { Hero } from "./hero";
+import { Hero } from "../components/home/hero";
+import { AboutMe } from "@/components/home/about-me";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <AboutMe />
     </main>
   );
 }
