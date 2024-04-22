4430fa85fae280d876ac0113ee42c721cb8a3883
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-04-22 23:41:09 +0630

    Updated the old portfolio page

--- src/data/Portfolio(Old).ts
+++ src/data/Portfolio(Old).ts
@@ -1,13 +1,13 @@
 import { Project } from "./models";
 
 export const Portfolio = new Project(
-  "Portfolio Website",
+  "Portfolio (Old) Website",
   "/images/Portfolio/Portfolio.png",
   "Portfolio website showcasing all my projects",
   "/",
   `<h2>Description</h2>
     <p>
-        This is my portfolio website I designed in the first week of January 2023. I made it with an intention of digitalising all my previous projects
+        This is my first portfolio website I designed in the first week of January 2023. I made it with an intention of digitalising all my previous projects
         and showcasing them for future references. 
      </p>
 
@@ -18,6 +18,11 @@ export const Portfolio = new Project(
         The entire website was just a small single page application coded with svelte.js. <br/>
         This is directly hosted in vercel in order for everyone to browse to.
      </p>
+
+     <img src="/images/Portfolio/Portfolio-Project-List.png"/>
+     <img src="/images/Portfolio/Portfolio-Project-1.png"/>
+     <img src="/images/Portfolio/Portfolio-Project-2.png"/>
+     <img src="/images/Portfolio/Portfolio-Project-3.png"/>
      `,
   ["html", "css", "svelte", "vercel"],
 );
