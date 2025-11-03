63a6cd33689cb691e04cc669da6285eeb798c539
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-11-02 19:46:11 -0500

    Removed unnecessary code

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,8 +1,6 @@
 import { Hero } from "@/components/home/hero";
 import { AboutMe } from "@/components/home/about-me";
 import { Projects } from "@/components/home/projects";
-import { UofTWebring } from "@/components/uoftwebring";
-
 import "./App.css";
 import useWindowDimensions from "@/utils/use-window-dimensions";
 
@@ -15,9 +13,6 @@ function App() {
             <Hero />
             { width > mobileBreakpoint && <AboutMe /> }
             <Projects />
-            <div className="hidden">
-                <UofTWebring />
-            </div>
             { width <= mobileBreakpoint && <AboutMe /> }
         </main>
     );
